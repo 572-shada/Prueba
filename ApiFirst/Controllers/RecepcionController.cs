a8ddbf1316f0d201295e92fed43cc671e3a7f826
Sharen-Quimbayo <sdquimbayo@misena.edu.co>
2021-09-29 19:35:35 -0500

    My primera WebAppi con los metodos Http

---
+++ ApiFirst/Controllers/RecepcionController.cs
@@ -0,0 +1,58 @@
+﻿using FirstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFirst.Controllers
+{
+
+    [Route("api/Recepcion")]
+    [ApiController]
+    public class RecepcionController : ControllerBase
+
+    {
+        private List<Peliculas> Peliculas { get; set; } = new List<Peliculas>() {
+        new Peliculas(){
+        Id = 123,
+        NombrePelicula = "El castillo ambulante",
+        Estudio = "Ghibli"
+
+         }
+        };
+
+        [HttpGet]
+        public List<Peliculas> Get()
+        {
+            return Peliculas;
+        }
+
+        [HttpPut("{id}")]
+        public Peliculas Update([FromBody] Peliculas peliculasUp, [FromQuery] int id)
+        {
+            var peliculas = Peliculas.Find(peliculas => peliculas.Id == id);
+            peliculas.NombrePelicula = peliculasUp.NombrePelicula;
+            peliculas.Estudio = peliculasUp.Estudio;
+            return peliculas;
+        }
+
+        [HttpPost]
+        public Peliculas Update([FromBody] Peliculas peliculas)
+        {
+            Peliculas.Add(peliculas);
+            return peliculas;
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public Peliculas Delete([FromQuery] int id)
+        {
+            var peliculas = Peliculas.Find(peliculas => peliculas.Id == id);
+            Peliculas.Remove(peliculas);
+            return peliculas;
+        }
+    }
+}
+
+    
---
+++ ApiFirst/Models/Peliculas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstApp.Models
+{
+    public class Peliculas
+    {
+        public int Id { get; set; }
+        public String NombrePelicula { get; set; }
+        public String Estudio { get; set; }
+
+    }
+}
